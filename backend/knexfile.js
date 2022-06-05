035caea112ea25cc3473da38300363810bdd7c9e
Junior Barros <juniorbaarros@gmail.com>
2022-06-05 16:51:46 -0300

    fix: updated knex version - fix error in migrations

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -6,7 +6,6 @@ module.exports = {
   pool: {
     min: 2,
     max: 10,
-    propagateCreateError: false, // <- default is true, set to false
   },
   migrations: {
     tableName: "knex_migrations",
